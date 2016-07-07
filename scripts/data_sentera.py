2d3aeb1fe58592075364c7987239b76cb9011cc6
Curtis L. Olson <olson126@umn.edu>
2016-07-07 12:08:35 -0500

    Work out the correct axis remapping for the IMU sensor data. Sentera IMU is mounted 'backwards' and 'upside down' relative to the conventions I am used to using.

--- scripts/data_sentera.py
+++ scripts/data_sentera.py
@@ -2,6 +2,8 @@
 
 import fileinput
 import math
+import numpy as np
+import os
 import re
 
 import navpy
@@ -27,6 +29,21 @@ def load(flight_dir):
     imu_file = flight_dir + "/imu.csv"
     gps_file = flight_dir + "/gps.csv"
 
+    # calibration by plotting and eye-balling (just finding center point, no
+    # normalization cooked into calibration.)
+    #hx_coeffs = np.array([ 1.0,  -1.5], dtype=np.float64)
+    #hy_coeffs = np.array([ 1.0, -78.5], dtype=np.float64)
+    #hz_coeffs = np.array([ 1.0, -156.5], dtype=np.float64)
+    
+    # calibration based on first iteration magEKF output
+    hx_coeffs = np.array([ 0.01857768, -0.18006511], dtype=np.float64)
+    hy_coeffs = np.array([ 0.01856929, -1.20853805], dtype=np.float64)
+    hz_coeffs = np.array([ 0.01559641,  2.81011594], dtype=np.float64)
+   
+    hx_func = np.poly1d(hx_coeffs)
+    hy_func = np.poly1d(hy_coeffs)
+    hz_func = np.poly1d(hz_coeffs)
+
     fimu = fileinput.input(imu_file)
     for line in fimu:
         #print line
@@ -36,13 +53,25 @@ def load(flight_dir):
             if len(tokens) == 11 and isFloat(tokens[10]):
                 #print '"' + tokens[10] + '"'
                 (time, p, q, r, ax, ay, az, hx, hy, hz, temp) = tokens
-                hx = float(hx) + 13
-                hy = float(hy) - 63
-                hz = float(hz) - 80
+                # remap axis before applying mag calibration
+                p = -float(p)
+                q =  float(q)
+                r = -float(r)
+                ax = -float(ax)*g
+                ay =  float(ay)*g
+                az = -float(az)*g
+                hx = -float(hx)
+                hy =  float(hy)
+                hz = -float(hz)
+                hx_new = hx_func(float(hx))
+                hy_new = hy_func(float(hy))
+                hz_new = hz_func(float(hz))
+                norm = np.linalg.norm([hx_new, hy_new, hz_new])
+                hx_new /= norm
+                hy_new /= norm
+                hz_new /= norm
                 imu = pydefs.IMU( float(time)/1000000.0, 0,
-                                  float(p), float(q), float(r),
-                                  float(ax)*g, float(ay)*g, float(az)*g,
-                                  float(hx), float(hy), float(hz),
+                                  p, q, r, ax, ay, az, hx_new, hy_new, hz_new,
                                   float(temp) )
                 imu_data.append( imu )
 
@@ -71,3 +100,19 @@ def load(flight_dir):
     print "gps records:", len(gps_data)
 
     return imu_data, gps_data, filter_data
+
+def save_filter_post(flight_dir, t_store, data_store):
+    filename = os.path.join(flight_dir, 'filter-post.txt')
+    f = open(filename, 'w')
+    size = len(t_store)
+    for i in range(size):
+        line = "%.3f,%.10f,%.10f,%.2f,%.4f,%.4f,%.4f,%.2f,%.2f,%.2f,0" % \
+               (t_store[i],
+                data_store.nav_lat[i]*180.0/math.pi,
+                data_store.nav_lon[i]*180.0/math.pi,
+                data_store.nav_alt[i], data_store.nav_vn[i],
+                data_store.nav_ve[i], data_store.nav_vd[i],
+                data_store.phi[i]*180.0/math.pi,
+                data_store.the[i]*180.0/math.pi,
+                data_store.psi[i]*180.0/math.pi)
+        f.write(line + '\n')
