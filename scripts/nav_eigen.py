d9304612f07f527ff77a34138857d09553910b28
Curtis L. Olson <curtolson@gmail.com>
2017-09-06 12:21:13 -0500

    aura-flightdata loads and builds structures with native/simple python code. To pass these structures to C++ code we need to copy them element by element to the corresponding C++ version of the structure which is defined in this project and installed as the nav.structs module.

--- scripts/nav_eigen.py
+++ scripts/nav_eigen.py
@@ -4,7 +4,36 @@ import sys
 
 import nav.EKF15
 
+def mkIMUdata( src ):
+    result = nav.structs.IMUdata()
+    result.time = src.time
+    result.p = src.p
+    result.q = src.q
+    result.r = src.r
+    result.ax = src.ax
+    result.ay = src.ay
+    result.az = src.az
+    result.hx = src.hx
+    result.hy = src.hy
+    result.hz = src.hz
+    result.temp = src.temp
+    return result
+
+def mkGPSdata( src ):
+    result = nav.structs.GPSdata()
+    result.time = src.time
+    result.lat = src.lat
+    result.lon = src.lon
+    result.alt = src.alt
+    result.vn = src.vn
+    result.ve = src.ve
+    result.vd = src.vd
+    result.sats = src.sats
+    result.newData = src.newData
+    return result
+    
 class filter():
+    
     def __init__(self):
         self.ekf = nav.EKF15.EKF15()
         self.name = 'EKF15'
@@ -13,11 +42,15 @@ class filter():
         self.ekf.set_config(config)
         
     def init(self, imu, gps, filterpt=None):
-        nav = self.ekf.init(imu, gps)
+        Cimu = mkIMUdata( imu )
+        Cgps = mkGPSdata( gps )
+        nav = self.ekf.init(Cimu, Cgps)
         return nav
 
     def update(self, imu, gps, filterpt=None):
-        nav = self.ekf.update(imu, gps)
+        Cimu = mkIMUdata( imu )
+        Cgps = mkGPSdata( gps )
+        nav = self.ekf.update(Cimu, Cgps)
         return nav
 
     def close(self):
